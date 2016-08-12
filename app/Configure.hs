848c4dc52fd5599b001a5c5b1e6217e7538cfd4f
oisdk <oisin.kidney@gmail.com>
2016-08-12 20:17:51 +0100

    working executable

--- app/Configure.hs
+++ app/Configure.hs
@@ -2,19 +2,20 @@
 
 module Main where
 
-import qualified Data.Text.Lazy.IO    as T
+import qualified Data.Text.Lazy.IO         as T
 import           Experiments
+import           Filesystem.Path.CurrentOS
 import           SBML
 import           System.Exit
 import           Text.Taggy.Renderer
 import           Text.Trifecta.Parser
-import           Turtle               (optPath, options)
+import           Turtle                    (optPath, options)
 
 main :: IO ()
 main = do
   (p,s,c) <- options "SSystem" $ (,,) <$> optPath "Problem" 'p' "Path to problem file"
                                       <*> optPath "SBML-out" 's' "Path to sbml output"
                                       <*> optPath "Python-out" 'c' "Path to python output"
-  (ss,df) <- maybe exitFailure pure =<< parseFromFile problem (show p)
-  T.writeFile (show c) (toExpFormat df)
-  T.writeFile (show s) ((render . toSBML . fmap Right) ss)
+  (ss,df) <- maybe exitFailure pure =<< parseFromFile problem (encodeString p)
+  T.writeFile (encodeString c) (toExpFormat df)
+  T.writeFile (encodeString s) ((render . toSBML . fmap Right) ss)
